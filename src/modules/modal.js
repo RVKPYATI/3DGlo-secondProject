973ab0a175015ac212068a8c68467f6aa4389d58
Vasiliy Rusanov <rvk1986@mail.ru>
2022-11-12 11:47:00 +0300

    Добавил анимацию окна

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,16 +2,35 @@ const modal = () => {
     const buttons = document.querySelectorAll('.popup-btn');
     const modal = document.querySelector('.popup');
     const btnPopalClose = modal.querySelector('.popup-close');
+    const popupContent = modal.querySelector('.popup-content');
+    let step = 0;
+
+    const modalAnimation = () => {
+
+        step += 2;
+        popupContent.style.left = '-38%';
+        popupContent.style.left = 0 + step + '%';
+        let anim = requestAnimationFrame(modalAnimation);
+        if (step >= 38) {
+            cancelAnimationFrame(anim);
+            step = 0;
+        }
+
+    };
 
     buttons.forEach(button => {
-        button.addEventListener('click', ()=> {
+        button.addEventListener('click', () => {
             modal.style.display = 'block';
+            modalAnimation();
         });
     });
 
     btnPopalClose.addEventListener('click', () => {
         modal.style.display = 'none';
+
     });
+
+
 };
 
 export default modal;
