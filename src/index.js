8f76bab599161b3e7698f1ff09a350f6a3ae34bd
Vasiliy Rusanov <rvk1986@mail.ru>
2022-12-01 15:15:38 +0300

    start lesson27

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import calc from './modules/calc';
 import validate from './modules/validate';
 import { tabs } from './modules/tabs';
 import { slider } from './modules/slider';
+import { sendForm } from './modules/sendForm';
 
 timer('31 december 2022');
 menu();
@@ -13,4 +14,5 @@ calc();
 validate();
 tabs();
 slider();
+sendForm('#form1');
 
