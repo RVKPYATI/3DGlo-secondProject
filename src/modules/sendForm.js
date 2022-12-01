b00feec4a9ee9c8dde36a0c3290048aabbc54a2a
Vasiliy Rusanov <rvk1986@mail.ru>
2022-12-01 15:16:24 +0300

    Start lesson27

---
+++ src/modules/sendForm.js
@@ -0,0 +1,27 @@
+export const sendForm = (idForm) => {
+    const form = document.querySelector(idForm);
+    
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        console.log('dfd')
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
